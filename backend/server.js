64511b4d036b3e0427c978654958ed59fdecba22
Karthika <karthi205c@gmail.com>
2026-03-24 12:21:54 +0530

    Configure explicit CORS origins

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,20 @@ const connectDB = require('./config/db');
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://online-banking-management-system-t63rkwu87-karthika-cs-projects.vercel.app',
+    'https://online-banking-management-system-sable.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
-app.use('/api/auth',         require('./routes/authRoutes'));
-app.use('/api/users',        require('./routes/userRoutes'));
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/transactions', require('./routes/transactionRoutes'));
 
 app.get('/', (req, res) => res.json({ message: '🏦 Banking API Running' }));
